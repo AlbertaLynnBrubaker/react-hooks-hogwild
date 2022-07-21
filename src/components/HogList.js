10488069aa98a47aadd16d64e5e9a0fe5ca44a6f
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-21 08:55:48 -0500

    -

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -2,7 +2,20 @@ import React from "react";
 
 function HogList({hogs}) {
 
-  console.log(hogs)
+  const hogArray = hogs.map(hog => {
+
+    return (
+      <>
+        <h3>Name: {hog.name}</h3>
+        <p>specialty: {hog.specialty}</p>
+        <p>greased: {}</p>
+    weight: 2.0,
+    "highest medal achieved": "bronze",
+    image:
+      </>
+    )
+  })
+
 	return (
 		<div>
 			
