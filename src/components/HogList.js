71650fe69d44d252ef79246adf8f9b163e241a33
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-07-21 11:49:58 -0500

    add filter for greased

--- src/components/HogList.js
+++ src/components/HogList.js
@@ -2,19 +2,40 @@ import React, {useState} from "react";
 import HogTile from "./HogTile";
 
 function HogList({hogs}) {
+  const [selectGreased, setSelectGreased] = useState('All')
 
-  const hogArray = hogs.map(hog => {
+  const filteredHogs = hogs.filter(hog => {
+    if (selectGreased === "All") {
+      return true;
+    } else if (selectGreased === "Greased") {
+      return hog.greased === true;
+    } else if (selectGreased === "Not Greased") {
+      return hog.greased === false;
+    }
+  })
+
+  const displayHogs = filteredHogs.map(hog => {
 
     return (
       <HogTile key= {hog.name} hog= {hog}/>
     )
   })
 
+  const handleFilter = (e) => {
+    setSelectGreased(e.target.value)
+  }
 
 	return (
     <div>
-        <div className="ui eight stackable cards">{hogArray}</div>
-        <div></div>
+      <div id="filter">
+        <select name="filter" onChange={handleFilter}>
+          <option value="All" default>All</option>
+          <option value="Greased">Greased</option>
+          <option value="Not Greased">Not Greased</option>
+        </select>
+      </div>
+      <div className="ui eight stackable cards">{displayHogs}</div>
+      
     </div>
 	);
 }
